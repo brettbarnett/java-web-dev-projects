9286c02cf9f126642c9db99fc4aa3f60cfeea094
Brett Barnett <brett_barnett@sbcglobal.net>
2024-04-29 13:51:15 -0500

    Completed Chapter 9 Exercise: Exceptions

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,10 +1,13 @@
 package org.launchcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        int division = Divide(10, 0);
+        System.out.println(division);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -13,15 +16,42 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for (Map.Entry<String, String> keyValuePair : studentFiles.entrySet()) {
+            System.out.println(keyValuePair.getKey() + " got a score of " + CheckFileExtension(keyValuePair.getValue()));
+        }
     }
 
-    public static void Divide(int x, int y)
+    public static int Divide(int x, int y)
     {
-        // Write code here!
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("Cannot divide by zero!");
+            }
+            catch (ArithmeticException e) {
+                e.printStackTrace();
+                return -1;
+            }
+        }
+        else {
+            return x / y;
+        }
     }
 
     public static int CheckFileExtension(String fileName)
     {
-        // Write code here!
+        if (fileName == null || fileName.isEmpty()) {
+            try {
+                throw new IllegalArgumentException("fileName cannot be null or an empty string!");
+            }
+            catch (IllegalArgumentException e) {
+                e.printStackTrace();
+                return -1;
+            }
+        }
+
+        if (fileName.endsWith(".java")) {
+            return 1;
+        }
+        return 0;
     }
 }
